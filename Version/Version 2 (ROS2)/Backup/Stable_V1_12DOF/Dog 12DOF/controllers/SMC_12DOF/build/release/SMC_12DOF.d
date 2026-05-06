b4e7d09c7463eddf2c743235d498b87e623d2b4d
Tuanlinh05 <vutlinh.work@gmail.com>
2026-05-06 23:47:40 +0700

    Fix

--- Version/Version 2 (ROS2)/Backup/Stable_V1_12DOF/Dog 12DOF/controllers/SMC_12DOF/build/release/SMC_12DOF.d
+++
@@ -1,6 +0,0 @@
-build/release/SMC_12DOF.o: SMC_12DOF.c \
- D:\Webots/include/controller/c/webots/robot.h \
- D:\Webots/include/controller/c/webots/types.h \
- D:\Webots/include/controller/c/webots/nodes.h \
- D:\Webots/include/controller/c/webots/motor.h \
- D:\Webots/include/controller/c/webots/position_sensor.h
